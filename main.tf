ad47dc47caf644f529eadc6acf292162067a4efc
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-08-07 16:17:50 +0530

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID for us-east-1 region
+  instance_type = "t2.micro"               # Change this to your desired instance type
+
+  tags = {
+    Name = "ExampleEC2Instance"
+  }
+}

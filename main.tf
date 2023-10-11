baf0a28577e7d434fdb1d38f21b2f247df103bd5
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-10-11 11:18:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"  # Change this to your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "demo_instance" {
   count         = 3
   ami           = "ami-0ded8326293d3201b"  
   instance_type = "t2.micro"               # Change this to your desired instance type

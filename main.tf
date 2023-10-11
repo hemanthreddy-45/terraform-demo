6d32c93f82d940bf286828d3d73db41379ea52e1
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-10-11 11:13:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"  # Change this to your desired AWS region
+  region = "us-east-2"  # Change this to your desired AWS region
 }
 
 resource "aws_instance" "example" {
+  count         = 3
   ami           = "ami-0ded8326293d3201b"  
   instance_type = "t2.micro"               # Change this to your desired instance type
 
